ba1ba360269001b95ae1909bbc6d2d8b3734c010
CyntiBinti <cynthia_m@live.com>
2021-08-20 23:51:41 +0100

    new js file challenge

--- file.js
+++ file.js
@@ -3,3 +3,8 @@ function capitalise(string) {
 }
 
 capitalise("UPPERCASE")
+
+/* Task: Write a function called capitalize that takes a
+string and returns that string with only the first
+letter capitalized. Make sure that it can take strings
+that are lowercase, UPPERCASE or BoTh*/
---
+++ file_2.js
@@ -0,0 +1,9 @@
+function lastLetter(string) {
+    console.log(string.charAt(string.length-1))
+}
+
+lastLetter("All the lights")
+
+/* Task: Write a function called lastLetter that takes
+a string and returns the very last letter of that
+string: lastLetter("abcd") should return "d" */
